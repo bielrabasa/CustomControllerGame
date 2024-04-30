1e2e70acaf47ccb2dd804670a0b244f692a22afb
Biel Rabasa <bielrabasa@gmail.com>
2024-05-01 00:32:19 +0200

    correct movement

--- Assets/BaseFlight.cs
+++ Assets/BaseFlight.cs
@@ -1,10 +1,14 @@
+using Microsoft.Win32;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BaseFlight : MonoBehaviour
 {
-    float percentage;
+    public float startVelocity = 10;
+
+    public float steerForce = 20;
+    public float turnForce = 10;
 
     Rigidbody rb;
     Vector3 rot;
@@ -13,34 +17,28 @@ public class BaseFlight : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         rot = transform.eulerAngles;
+
+        rb.velocity = Vector3.forward * startVelocity;
     }
 
 
     void Update()
     {
-        rot.x += 20 * Input.GetAxis("Vertical") * Time.deltaTime;
+        rot.x += steerForce * Input.GetAxis("Vertical") * Time.deltaTime;
         rot.x = Mathf.Clamp(rot.x, -30, 45);
 
-        rot.y += 20 * Input.GetAxis("Horizontal") * Time.deltaTime;
+        float axisH = Input.GetAxis("Horizontal");
+        rot.y += steerForce * axisH * Time.deltaTime;
 
-        rot.z = -5 * Input.GetAxis("Horizontal");
-        rot.z = Mathf.Clamp(rot.z, -5, 5);
+        //if(axisH < 0.1f) rot.z //TODO: Tornar a 0 gradualment
+        rot.z += -turnForce * axisH * Time.deltaTime;
+        rot.z = Mathf.Clamp(rot.z, -10, 10);
+
+
 
         transform.rotation = Quaternion.Euler(rot);
 
-        //percentage = rot.x / 45f;
-
-        //float mod_drag = (percentage * -2) + 6;
-        //float mod_speed = percentage * (13.8f - 12.5f) + 12.5f; //TODO: Fixed, change
-
-        //rb.drag = mod_drag;
-        Vector3 localV = transform.InverseTransformDirection(rb.velocity);
-
-        //localV.z = mod_speed; // - (Vector3.Dot(transform.up, Vector3.up) * Time.deltaTime);
-        localV.z += (Vector3.Dot(transform.up, Vector3.up) * 100f * Time.deltaTime);
-
-        Vector3 vel = transform.TransformDirection(localV);
-
-        rb.velocity = vel;
+        float dot = Vector3.Dot(transform.forward, Vector3.up);
+        rb.velocity = transform.forward * (rb.velocity.magnitude - dot * Time.deltaTime);
     }
 }
